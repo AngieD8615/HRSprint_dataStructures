b7db0684b74b01b4da255fd78d351654117475ae
AngieD8615 <adavidson1@gmail.com>
2020-10-08 22:32:31 -0600

    partially complete binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,60 @@
-var BinarySearchTree = function(value) {
+var BinarySearchTree = function (value) {
+  var obj = Object.create(biTreeMethods);
+
+  obj.value = value;
+  obj.left = null;
+  obj.right = null;
+  return obj;
 };
+
+var biTreeMethods = {};
+
+
+biTreeMethods.insert = function (value) {
+  let newNode = BinarySearchTree(value);
+  if (value < this.value && this.left === null) {
+    this.left = newNode;
+  }
+  if (value > this.value && this.right === null) {
+    this.right = newNode;
+  }
+  if (value < this.value && this.left !== null) {
+    let leftNode = this.left;
+    leftNode.insert(value);
+  }
+  if (value > this.value && this.right !== null) {
+    let rightNode = this.right;
+    rightNode.insert(value);
+  }
+
+},
+
+biTreeMethods.contains = function (target) {
+  if (target === this.value) {
+    return true;
+  }
+  if (target < this.value) {
+    if (this.left === null) {
+      return false;
+    }
+    let leftNode = this.left;
+    return leftNode.contains(target);
+  }
+  if (target > this.value) {
+    if (this.right === null) {
+      return false;
+    }
+    let rightNode = this.right;
+    return rightNode.contains(target);
+  }
+
+},
+
+biTreeMethods.depthFirstLog = function (cb) {
+
+};
+
+
 
 
 /*
